4dd19228bab48f4aaec18ae84de7b71f44fbdd95
sagbeyeg <agbeyegbesisan@gmail.com>
2020-09-28 12:25:52 -0400

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,72 @@ class Song
   @@all = []
 
   def self.all
-    @@all
+    @@all 
   end
 
   def save
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.save
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    # if self.find_by_name(song_name) == nil
+    #   self.create_by_name(song_name)
+    # else 
+    #   self.find_by_name(song_name)
+    # end
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_artist = filename.split(" - ")
+    artist_name = song_artist[0]
+    song_name = song_artist[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_artist = filename.split(" - ")
+    artist_name = song_artist[0]
+    song_name = song_artist[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
